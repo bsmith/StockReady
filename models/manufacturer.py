ac2263782274addacd286181963af6133d5f0edf
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-13 09:43:21 +0000

    ✅ Implement model Manufacturer & pass tests

--- models/manufacturer.py
+++ models/manufacturer.py
@@ -1,3 +1,12 @@
 class Manufacturer:
-    def __init__(*args, **kwargs):
-        pass
+    def __init__(self, full_name, short_brand_name,
+            trade_website=None, trade_telephone=None,
+            customer_website=None, customer_telephone=None,
+            id=None):
+        self.full_name = full_name
+        self.short_brand_name = short_brand_name
+        self.trade_website = trade_website
+        self.trade_telephone = trade_telephone
+        self.customer_website = customer_website
+        self.customer_telephone = customer_telephone
+        self.id = id
