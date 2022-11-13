613185d561fc20fcf475715f011841c262852fc6
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-13 15:46:56 +0000

    🚧 Code for controllers (manufacturers and products)

--- controllers/manufacturers_controller.py
+++ controllers/manufacturers_controller.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect
+
+from models.manufacturer import Manufacturer
+import repositories.manufacturer_repository as manufacturer_repository
 
 manufacturers_blueprint = Blueprint("manufacturers", __name__)
 
@@ -14,34 +17,54 @@ manufacturers_blueprint = Blueprint("manufacturers", __name__)
 # Not needed for MVP
 # @manufacturers_blueprint.route('/manufacturers/')
 # def manufacturers():
-#     # ... manufacturers_repository.select_all() ...
-#     return render_template('manufacturers/index.html.j2')
+#     all_manufacturers = manufacturer_repository.select_all()
+#     return render_template('manufacturers/index.html.j2', all_manufacturers=all_manufacturers)
 
 @manufacturers_blueprint.route('/manufacturers/new')
-def new_product():
+def new_manufacturer():
     return render_template('manufacturers/new.html.j2')
 
+def _convert_zero_length_str_to_none(string):
+    return None if len(string) == 0 else string
+
+def _validate_manufacturer_form(request):
+    validated = {}
+    validated['full_name'] = request['full_name']
+    validated['short_brand_name'] = request['short_brand_name']
+    validated['trade_website'] = _convert_zero_length_str_to_none(request['trade_website'])
+    validated['trade_telephone'] = _convert_zero_length_str_to_none(request['trade_telephone'])
+    validated['customer_website'] = _convert_zero_length_str_to_none(request['customer_website'])
+    validated['customer_telephone'] = _convert_zero_length_str_to_none(request['customer_telephone'])
+    return validated
+
 @manufacturers_blueprint.route('/manufacturers', methods=["POST"])
-def create_product():
-    # ... manufacturers_repository.save(product) ...
-    raise NotImplementedError()
+def create_manufacturer():
+    validated = _validate_manufacturer_form(request)
+    new_manufacturer = Manufacturer(**validated)
+    manufacturer_repository.save(new_manufacturer)
+    return redirect(url_for('.show_manufacturer', id=new_manufacturer.id))
 
 @manufacturers_blueprint.route('/manufacturers/<int:id>')
-def show_product(id):
-    # ... manufacturers_repository.select(id) ...
-    return render_template('manufacturers/show.html.j2')
+def show_manufacturer(id):
+    manufacturer = manufacturer_repository.select(id)
+    return render_template('manufacturers/show.html.j2', manufacturer=manufacturer)
 
 @manufacturers_blueprint.route('/manufacturers/<int:id>/edit')
-def edit_product(id):
-    # ... manufacturers_repository.select(id) ...
-    return render_template('manufacturers/edit.html.j2')
+def edit_manufacturer(id):
+    manufacturer = manufacturer_repository.select(id)
+    return render_template('manufacturers/edit.html.j2', manufacturer=manufacturer)
 
 @manufacturers_blueprint.route('/manufacturers/<int:id>', methods=["POST"])
-def update_product(id):
-    # ... manufacturers_repository.update(product) ...
-    raise NotImplementedError()
+def update_manufacturer(id):
+    manufacturer = manufacturer_repository.select(id)
+    validated = _validate_manufacturer_form(request)
+    # merge the validated data into the object
+    for key, value in validated.items:
+        setattr(manufacturer, key, value)
+    manufacturer_repository.update(manufacturer)
+    return redirect(url_for('.show_manufacturer', id=manufacturer.id))
 
 @manufacturers_blueprint.route('/manufacturers/<int:id>/delete', methods=["POST"])
-def delete_product(id):
-    # ... manufacturers_repository.delete(id) ...
-    raise NotImplementedError()
+def delete_manufacturer(id):
+    manufacturer_repository.delete(id)
+    return redirect(url_for('.manufacturers'))
--- controllers/products_controller.py
+++ controllers/products_controller.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, render_template
+from decimal import Decimal
+
+from flask import Blueprint, render_template, request, url_for, redirect
+
+from models.product import Product
+import repositories.product_type_repository as product_type_repository
+import repositories.manufacturer_repository as manufacturer_repository
+import repositories.product_repository as product_repository
 
 products_blueprint = Blueprint("products", __name__)
 
@@ -13,34 +20,60 @@ products_blueprint = Blueprint("products", __name__)
 
 @products_blueprint.route('/products/')
 def products():
-    # ... products_repository.select_all() ...
-    return render_template('products/index.html.j2')
+    all_products = product_repository.select_all()
+    return render_template('products/index.html.j2', all_products=all_products)
 
 @products_blueprint.route('/products/new')
 def new_product():
-    return render_template('products/new.html.j2')
+    all_product_types = product_type_repository.select_all()
+    all_manufacturers = manufacturer_repository.select_all()
+    return render_template('products/new.html.j2', all_product_types=all_product_types, all_manufacturers=all_manufacturers)
+
+def _validate_product_form(request):
+    validated = {}
+    validated['mpn'] = request['mpn']
+    validated['manufacturer'] = manufacturer_repository.select(request['manufacturer_id'])
+    validated['short_description'] = request['short_description']
+    validated['long_description'] = request['long_description']
+    if len(validated['long_description']) == 0:
+        validated['long_description'] = None
+    validated['product_type'] = product_type_repository.select(request['product_type_id'])
+    validated['screen_size'] = request['screen_size']
+    validated['stock_on_hand'] = request['stock_on_hand']
+    validated['cost_price'] = Decimal(request['cost_price'])
+    validated['retail_price'] = Decimal(request['retail_price'])
+    return validated
 
 @products_blueprint.route('/products', methods=["POST"])
 def create_product():
-    # ... products_repository.save(product) ...
-    raise NotImplementedError()
+    validated = _validate_product_form(request)
+    new_product = Product(**validated)
+    product_repository.save(new_product)
+    return redirect(url_for('.show_product', id=new_product.id))
 
 @products_blueprint.route('/products/<int:id>')
 def show_product(id):
-    # ... products_repository.select(id) ...
-    return render_template('products/show.html.j2')
+    product = product_repository.select(id)
+    return render_template('products/show.html.j2', product=product)
 
 @products_blueprint.route('/products/<int:id>/edit')
 def edit_product(id):
-    # ... products_repository.select(id) ...
-    return render_template('products/edit.html.j2')
+    product = product_repository.select(id)
+    all_product_types = product_type_repository.select_all()
+    all_manufacturers = manufacturer_repository.select_all()
+    return render_template('products/edit.html.j2', product=product, all_product_types=all_product_types, all_manufacturers=all_manufacturers)
 
 @products_blueprint.route('/products/<int:id>', methods=["POST"])
 def update_product(id):
-    # ... products_repository.update(product) ...
-    raise NotImplementedError()
+    product = product_repository.select(id)
+    validated = _validate_product_form(request)
+    # merge the validated data into the object
+    for key, value in validated.items:
+        setattr(product, key, value)
+    product_repository.update(product)
+    return redirect(url_for('.show_product', id=new_product.id))
 
 @products_blueprint.route('/products/<int:id>/delete', methods=["POST"])
 def delete_product(id):
-    # ... products_repository.delete(id) ...
-    raise NotImplementedError()
+    product_repository.delete(id)
+    return redirect(url_for('.products'))
