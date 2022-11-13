378eded1d608e04a9add3498bb06c6a18f1f7920
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-13 11:20:04 +0000

    🚧 Add `__repr__` to ProductType

--- models/product_type.py
+++ models/product_type.py
@@ -2,3 +2,9 @@ class ProductType:
     def __init__(self, name, id=None):
         self.name = name
         self.id = id
+
+    def __repr__(self):
+        if self.id:
+            return f"ProductType({self.name!r}, id={self.id!r})"
+        else:
+            return f"ProductType({self.name!r})"
