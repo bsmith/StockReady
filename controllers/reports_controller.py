65469f5dcd73e8810474738e2fd67fd358ee829b
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-15 15:05:04 +0000

    ♻️ Refactor financial reports to use a model object

--- controllers/reports_controller.py
+++ controllers/reports_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+from models.financial_report import FinancialReport
 import repositories.product_repository as product_repository
 
 reports_blueprint = Blueprint("reports", __name__)
@@ -8,17 +9,11 @@ reports_blueprint = Blueprint("reports", __name__)
 def reports():
     return render_template('reports/index.html.j2')
 
-def _calc_totals(dc_products):
-    total_cost = sum(p.cost_price for p in dc_products)
-    total_retail = sum(p.retail_price for p in dc_products)
-    return total_cost, total_retail
-
 @reports_blueprint.route("/reports/discontinued")
 def discontinued():
     dc_products = product_repository.select_discontinued()
-    total_cost, total_retail = _calc_totals(dc_products)
-    potential_profit = total_retail - total_cost
+    financial_report = FinancialReport()
+    financial_report.calc_report(dc_products)
     return render_template('reports/discontinued.html.j2',
-        total_cost=total_cost, total_retail=total_retail,
-        potential_profit=potential_profit,
+        financial_report=financial_report,
         products=dc_products)
